f2fc6f60bf96b2d02051ee542e14aa14d8ff1ee0
Joakim Åhlfeldt <jocke93.ahlfeldt@gmail.com>
2014-10-14 16:01:52 +0200

    Fixed resolution bug that caused runtime crashes; window resolution now adapts to your screen

--- squirrel_game/conf.lua
+++ squirrel_game/conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
     t.title = "SquirrelDemo"		-- The title of the window the game is in (string)
     t.author = "BoxIT Scrum 2"		-- The author of the game (string)
-    t.window.width = 1920			-- The window width (number)
-    t.window.height = 1080			-- The window height (number)
+    t.window.width = 0			-- The window width (number)
+    t.window.height = 0			-- The window height (number)
     t.window.fullscreen = true -- Enable fullscreen (boolean)
     t.window.vsync = false			-- Enable vertical sync (boolean)
 end
