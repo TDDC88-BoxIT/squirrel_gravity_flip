d2231cc08f832aa181327b4101e1fe8f34c13816
wangrq <dlazycat@gmail.com>
2014-10-15 20:12:59 +0200

    initial gravity interface

---
+++ squirrel_game/physic.lua
@@ -0,0 +1,33 @@
+
+mgravity = 10
+mspeed = 10
+mcurx = 10
+mcury = 10
+startx = 10
+starty = 10
+
+function reset(curx, cury, speed)
+  mcurx = x
+  mcury = y
+  mspeed = speed
+end
+
+function update(dt)
+  
+end
+
+function getX()
+  
+end
+
+function getY()
+  
+end
+
+function setSpeed()
+  
+end
+
+function gsetGravity(gravity)
+  mgravity = gravity
+end
