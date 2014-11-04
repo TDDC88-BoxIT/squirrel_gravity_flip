ac19eab5b1d6a2068bc39155a37c406ff875111b
Oskar Eriksson <osker390@student.liu.se>
2014-11-04 15:14:20 +0100

    level.lua now more general, needs commenting

--- game/level.lua
+++ game/level.lua
@@ -3,7 +3,8 @@
 --This needs to be changed to be more general overall
 Level = { 
   raw_level = nil,
-  version = nil
+  version = nil,
+  tiles = nil;
   
 }
 function Level.load_level (level_number)
@@ -11,37 +12,37 @@ function Level.load_level (level_number)
   Level.version = loaded_level.version
   Level.raw_level = loaded_level
   Level.width = loaded_level.width
+  Level.tiles = get_tiles()
 end
 
-function Level.get_floor()
-  tilesets = Level.raw_level.tilesets
+function get_tiles()
+  
   --Gets the floor layers, must be changed
   floor_layer_data = Level.raw_level.layers[1].data
-  floor_tileset = nil
-  floors = {}
- 
-  -- GET FLOOR TILE FROM LEVEL
-  for k,v in pairs(tilesets) do     
-    if v.name == "Floor" then
-      floor_tileset = tilesets[k]
-    end
-  end
+  tilesets = {}
+  tiles = {} 
   
-  for k,v in pairs(floor_layer_data) do  
-    if v == floor_tileset.firstgid then  -- DISTINGUISHING FLOOR TILES IN THE DATA     
-      
-      floor_tile = {
-        width = floor_tileset.tilewidth,
-        height = floor_tileset.tileheight,
+  -- GET FLOOR TILE FROM LEVEL
+  for k,v in pairs(Level.raw_level.tilesets) do 
+    tilesets[v.firstgid] = v
+  end   
+
+  
+  for k,firstgid in pairs(floor_layer_data) do
+    if firstgid ~= 0 then  -- DISTINGUISHING FLOOR TILES IN THE DATA
+      tile = {
+        width = tilesets[firstgid].tilewidth,
+        height = tilesets[firstgid].tileheight,
+        image = tilesets[firstgid].image,
         -- SETS X AND Y COORDINATED FOR FLOOR TILE?
         --Since k starts on 1 we need to sub 1 to be able to start at 0
-        x = ((k-1) % Level.raw_level.width) * floor_tileset.tilewidth,
-        y = (math.floor((k-1) / Level.raw_level.width)) * floor_tileset.tileheight
+        x = ((k-1) % Level.raw_level.width) * tilesets[firstgid].tilewidth,
+        y = (math.floor((k-1) / Level.raw_level.width)) * tilesets[firstgid].tileheight
       }
-      table.insert(floors, floor_tile)
+      table.insert(tiles, tile)
       
       
     end 
   end
-  return floors
+  return tiles
 end
