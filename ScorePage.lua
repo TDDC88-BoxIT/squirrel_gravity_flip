13f15b491694356a18de9b093e5d51718cacd30b
FadingMu <neptune2854@gmail.com>
2014-11-08 11:44:48 +0100

    Fix a bug.

--- ScorePage.lua
+++ ScorePage.lua
@@ -1,8 +1,6 @@
---ScoreBoard={}
+ScoreBoard={}
+--local ScoreBoard={{"HanxiaoA",100},{"OskarA",500},{"AmandaA",300},{"TedA",700},{"TiktokA",150},{"HanxiaoB",200},{"OskarB",1500},{"AmandaB",1300},{"TedB",1700},{"TiktokB",1150},{"test buddy",50}}--for test
 function ScorePage(name,score)
-  
-ScoreBoard={{"HanxiaoA",100},{"OskarA",500},{"AmandaA",300},{"TedA",700},{"TiktokA",150},{"HanxiaoB",200},{"OskarB",1500},{"AmandaB",1300},{"TedB",1700},{"TiktokB",1150},{"test buddy",50}}--for test
-
   for i=1,11 do
     if ScoreBoard[i]==nil then
         ScoreBoard[i]={name,score}
@@ -13,11 +11,9 @@ ScoreBoard={{"HanxiaoA",100},{"OskarA",500},{"AmandaA",300},{"TedA",700},{"Tikto
       return a[2] > b[2] 
   end
   table.sort(ScoreBoard, comp)
+  table.remove(ScoreBoard,11)
   
   for i=1, #(ScoreBoard) do
-    if i==11 then
-      break
-    end
     print(ScoreBoard[i][1],ScoreBoard[i][2])
   end
 end 
