393c025cf66ffd27fe7594fbff33f9af26e81223
Fredrik Rosenqvist <frero718@student.liu.se>
2014-11-13 17:02:36 +0100

    meerp

--- game/power_up.lua
+++ game/power_up.lua
@@ -3,7 +3,7 @@ function activate_power_up(pu_name)
 	if(pu_name=="powerup1") then
 
 	elseif(pu_name=="powerup2") then
-		game_score = 10001
+		game_score = game_score+10001
 	elseif(pu_name=="powerup3") then
 
 	elseif(pu_name=="powerup4") then
