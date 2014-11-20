cd5cc05a650ff24a586c5cbb5972fc1da3b277f1
wangrq <dlazycat@gmail.com>
2014-11-20 14:06:09 +0100

    Tiles display. Modification to make it show on the box properly

--- game/game.lua
+++ game/game.lua
@@ -267,7 +267,9 @@ THE TILES ARE DRAWN ON THEIR ORIGINAL X-POSITION - gameCounter
 function draw_tiles()
   local sf = nil
     for k,v in pairs(Level.tiles) do
-      if v.x-gameCounter+v.width>0 and v.visibility==true then
+      -- This code can't run properly on the box because the difference 
+      -- of screen:copyfrom function . Wait for further improvement
+      if v.x-gameCounter+v.width>0 and v.visibility==true and v.x-gameCounter+v.width<screen:get_width() + v.width then
         if v.gid == 9 then
           move_cloud(v)
         elseif v.gid == 10 then
