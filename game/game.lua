dc7fa2fdb3508acc751a9b9d66396f3ac3b8bdfa
FadingMu <neptune2854@gmail.com>
2014-11-23 04:54:48 +0100

    The bug gone after reboot the system, so reuse the new gravity version again

--- game/game.lua
+++ game/game.lua
@@ -38,7 +38,7 @@ function start_game(level_number,game_type,life)
   current_level = level_number --TO BE PLACED SOMEWHERE ELSE
   gameCounter=0
   current_game_type=game_type
-  Level.load_level(1,current_game_type)
+  Level.load_level(3,current_game_type)
   load_level_atttributes()
   load_background_if_needed()
 
@@ -202,39 +202,18 @@ function move_character_V2()
       player.cur_x = player.cur_x+0.5*gameSpeed -- RESETS THE CHARACTER TO player.start_xpos IF IS HAS BEEN PUSHED BACK AND DOESN'T HIT ANYTHING ANYMORE
   end
 
--- MOVE CHARACTER ON THE Y-AXIS
-  --[[S=(G*t^2)/2 
-    There has a rule that, F(t)=t^2, F(t)-F(t-1)=2t-1 
-    We can get S(t)-S(t-1)=(G*(2t-1))/2]]
-    for i=0, gameSpeed, 1 do
-      if direction_flag == "down" then 
-        player.new_y=player.cur_y+0.5*G*(2*i+1)
-      else
-        player.new_y=player.cur_y-0.5*G*(2*i+1)
-      end
-      if (player.new_y > upper_bound_y or player.new_y < lower_bound_y) then -- CHARACTER HAS GOTTEN OUT OF RANGE
-        get_killed()
-        break;
-      end
-      if hitTest(gameCounter, Level.tiles, player.cur_x, player.new_y, character.width, character.height)==nil or falling==1 then
-        player.cur_y = player.new_y -- MOVE CHARACTER DOWNWARDS IF IT DOESN'T HIT ANYTHING
-      else
-        break
-      end
-    end
-  end
-
-
   --[[ MOVE CHARACTER ON THE Y-AXIS
     S=(G*t^2)/2 
     There has a rule that, F(t)=t^2, F(t)-F(t-1)=2t-1 
     We can get S(t)-S(t-1)=(G*(2t-1))/2
-    
-  
+    ]]
   if direction_flag == "down" then 
-    player.new_y=player.cur_y+0.5*G*(2*Tcount+1)
+    player.new_y=player.cur_y+0.5*G*(Tcount+0.5+gameSpeed)--since 2t-1 is looks to slow at the beginning and too fast at the end, so use t-1 here
   else       
-    player.new_y=player.cur_y-0.5*G*(2*Tcount+1)
+    player.new_y=player.cur_y-0.5*G*(Tcount+0.5+gameSpeed)
+  end
+  if (player.new_y > upper_bound_y or player.new_y < lower_bound_y) then -- CHARACTER HAS GOTTEN OUT OF RANGE
+    get_killed()   
   end
   local W,H,B_T,B_B=hitTest(gameCounter, Level.tiles, player.cur_x, player.new_y, character.width, character.height)
   if W==nil or falling==1 then
@@ -250,7 +229,7 @@ function move_character_V2()
     end
   end     
 end
-]]
+
 function draw_score()
   --DRAWS SCORE
   if get_menu_state() == "gameover_menu" then -- Don't draw score on gameover menu
